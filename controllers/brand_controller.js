36d4c2d194d9aafe321f6ad36f1d2c808489cd67
NewMo01 <mo.medhat99@gmail.com>
2026-08-17 01:14:21 +0300

    modified:   controllers/brand_controller.js
    Changes not staged for commit:
    	modified:   controllers/category_controller.js
    	modified:   controllers/product_controller.js
    	modified:   controllers/subcategory_controller.js
    	modified:   helpers/features.js
    	modified:   routes/subcategory_router.js

--- controllers/brand_controller.js
+++ controllers/brand_controller.js
@@ -1,61 +1,25 @@
-const jsend = require("jsend");
 const Brand = require("../models/brand_model");
-const ApiFeatures = require("../helpers/features");
-const Err = require("../helpers/app_error");
-
+const handlerFactory = require("../helpers/handler_factory");
 //@Desc   create brand
 //@Route  POST api/v1/brands/
 //@Access Private
-exports.createBrand = async function (req, res) {
-  const brand = await Brand.create(req.body);
-  res.status(201).jsend.success(brand);
-};
+exports.createBrand = handlerFactory.createHandler(Brand);
 
 //@Desc   Get list of brands
 //@Route  GET api/v1/brands/
 //@Access Public
-exports.getBrands = async function (req, res) {
-  const query = new ApiFeatures(Brand, req.query)
-    .filter()
-    .sort()
-    .paginate()
-    .select().mongoQuery;
-  console.log(query);
-  const brands = await query;
-  res
-    .status(200)
-    .jsend.success({ result: brands.length, page: +req.query.page, brands });
-};
+exports.getBrands = handlerFactory.getAllHandler(Brand);
 
 //@Desc   Get specific brand
 //@Route  GET api/v1/brands/id
 //@Access Public
-exports.getBrand = async function (req, res) {
-  const { id } = req.params;
-  const brand = await Brand.findById(id);
-  res.status(200).jsend.success(brand);
-};
-
+exports.getBrand = handlerFactory.getOneHandler(Brand);
 //@Desc   update specific brand
 //@Route  PATCH api/v1/brands/id
 //@Access Private
-exports.updateBrand = async function (req, res, next) {
-  const { id } = req.params;
-  const brand = await Brand.findByIdAndUpdate(id, { $set: { ...req.body } });
-  if (!brand) {
-    return next(Err.create("brand not found", 404));
-  }
-  res.status(200).jsend.success("Done");
-};
+exports.updateBrand = handlerFactory.updateHandler(Brand);
 
 //@Desc   delete brand
 //@Route  DELETE api/v1/brands/id
 //@Access Private
-exports.deleteBrand = async function (req, res, next) {
-  const { id } = req.params;
-  const brand = await Brand.findByIdAndDelete(id);
-  if (!brand) {
-    return next(Err.create("brand not found", 404));
-  }
-  res.status(200).jsend.success("Done");
-};
+exports.deleteBrand = handlerFactory.deleteHandler(Brand);
