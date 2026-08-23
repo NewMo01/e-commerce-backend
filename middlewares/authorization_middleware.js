94b6fb3beffdb78c30b4ff2cf827376d054b5bfd
NewMo01 <mo.medhat99@gmail.com>
2026-08-23 21:21:21 +0300

    z

--- middlewares/authorization_middleware.js
+++ middlewares/authorization_middleware.js
@@ -2,8 +2,6 @@ const Err = require("../helpers/app_error");
 
 module.exports = (...roles) =>
   function (req, res, next) {
-    console.log(roles)
-    console.log(req.role);
     if (!roles.includes(req.role)) {
       return next(Err.create("You are not authorized", 400, true));
     }
--- routes/auth_router.js
+++ routes/auth_router.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/signup", validator.signupValidator, controller.signup);
 router.post("/login", validator.loginValidator, controller.login);
 router.post("/logout", controller.logout);
-router.post("/refresh-token", controller.updateAccessToken);
+router.post("/refresh", controller.updateAccessToken);
 
 module.exports = router;
