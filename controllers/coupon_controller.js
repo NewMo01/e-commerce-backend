db5a6301e0b63bafbc9f4e56a8aec8395211a9fb
NewMo01 <mo.medhat99@gmail.com>
2026-08-27 16:18:01 +0300

    coupon finished !!!!

--- controllers/coupon_controller.js
+++ controllers/coupon_controller.js
@@ -1,5 +1,6 @@
 const jsend = require("jsend");
 const AppFeatures = require("../helpers/features");
+const Err = require("../helpers/app_error");
 const Coupon = require("../models/coupon_model");
 
 exports.createCoupon = async function (req, res) {
@@ -7,10 +8,31 @@ exports.createCoupon = async function (req, res) {
   res.status(201).jsend.success(coupon);
 };
 
-exports.getCoupon = async function (req, res) {};
+exports.getCoupon = async function (req, res, next) {
+  const { code } = req.params;
+
+  const coupon = await Coupon.findOne({ code });
+  //validate coupon-----------------------
+  switch (true) {
+    case !coupon:
+      return next(Err.create("coupon not found", 404, true));
+
+    case req.user.id !== coupon.userId.toString():
+      return next(Err.create("coupon not available for that user", 400, true));
+
+    case !coupon.isActive:
+      return next(Err.create("coupon is not active", 400, true));
+
+    case coupon.expireDate <= Date.now():
+        coupon.isActive = false;
+        await coupon.save();
+      return next(Err.create("coupon is expired", 400, true));
+  }
+
+  res.status(200).jsend.success(coupon);
+};
 
 exports.getListOfCoupons = async function (req, res) {
-    console.log(req.query);
   const coupons = await new AppFeatures(Coupon, req.query)
     .filter()
     .sort()
@@ -18,4 +40,11 @@ exports.getListOfCoupons = async function (req, res) {
   res.status(200).jsend.success(coupons);
 };
 
-exports.deleteCoupon = async function (req, res) {};
+exports.deleteCoupon = async function (req, res,next) {
+  const { code } = req.params;
+  const coupon = await Coupon.findOneAndDelete({ code });
+  if (!coupon) {
+    return next(Err.create("coupon not found", 404, true));
+  }
+  res.status(200).jsend.success("Coupon is deleted");
+};
